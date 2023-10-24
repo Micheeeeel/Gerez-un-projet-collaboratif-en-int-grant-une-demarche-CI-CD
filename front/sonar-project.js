d946c04cc0e545dbeeda7645b6849ab7d04138a7
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-10-24 11:13:57 +0200

    Mise en place de l'analyse SonarQube en local pour le frontend

---
+++ front/sonar-project.js
@@ -0,0 +1,14 @@
+const sonarqubeScanner = require("sonarqube-scanner");
+sonarqubeScanner(
+  {
+    serverUrl: "http://localhost:9000",
+    token: "squ_a5177bdeab7985362d9985cd6354b2b75a81bd7d", // Ajoutez cette ligne
+    options: {
+      "sonar.sources": ".", // Indique que la racine du projet est le point de départ de l'analyse.
+      "sonar.inclusions": "src/**", // Spécifie que seuls les fichiers sous le répertoire packages/core/src/ doivent être inclus dans l'analyse.
+      "sonar.javascript.lcov.reportPaths":
+        "coverage/bobapp/report-lcovonly.txt",
+    },
+  },
+  () => {}
+);
